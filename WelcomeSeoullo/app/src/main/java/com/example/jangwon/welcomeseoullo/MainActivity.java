0df5060ca8502e2a35ca0f5cf8d980d8e412cc83
ninetyfivejae <jh33836782@gmail.com>
2017-09-11 06:33:16 +0900

    Turning off application with clicking back button twice.
    Made not to construct same fragment, if same bottomNavigationView item is selected.

--- WelcomeSeoullo/app/src/main/java/com/example/jangwon/welcomeseoullo/MainActivity.java
+++ WelcomeSeoullo/app/src/main/java/com/example/jangwon/welcomeseoullo/MainActivity.java
@@ -10,8 +10,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final long FINISH_INTERVAL_TIME = 2000;
+    private long backPressedTime = 0;
 
     private TextView textHome;
     private TextView textRoute;
@@ -19,7 +23,10 @@ public class MainActivity extends AppCompatActivity {
     private TextView textFacility;
     private TextView textSettings;
 
-    Fragment fr;
+    Fragment fragment;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+    int currentMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,82 +42,109 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
 
+        fragment = new CarFragment();
+        fragmentManager = getFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main_fragment_place, fragment);
+        fragmentTransaction.commit();
+        currentMenu = bottomNavigationView.getSelectedItemId();
+
         bottomNavigationView.setOnNavigationItemSelectedListener(
                 new BottomNavigationView.OnNavigationItemSelectedListener() {
                     @Override
                     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                        fr = null;
+                        fragment = null;
                         switch (item.getItemId()) {
                             case R.id.action_home:
-                                textHome.setVisibility(View.VISIBLE);
-                                textRoute.setVisibility(View.GONE);
-                                textAR.setVisibility(View.GONE);
-                                textFacility.setVisibility(View.GONE);
-                                textSettings.setVisibility(View.GONE);
+                                if(currentMenu == item.getItemId()){
+                                    Toast.makeText(getApplicationContext(), "이미 고른 메뉴", Toast.LENGTH_SHORT).show();
+                                }
+                                else{
+                                    textHome.setVisibility(View.VISIBLE);
+                                    textRoute.setVisibility(View.GONE);
+                                    textAR.setVisibility(View.GONE);
+                                    textFacility.setVisibility(View.GONE);
+                                    textSettings.setVisibility(View.GONE);
 
-                                fr = new CarFragment();
+                                    fragment = new CarFragment();
+                                    currentMenu = item.getItemId();
+                                    switchFragment();
+                                }
                                 break;
                             case R.id.action_route:
-                                textHome.setVisibility(View.GONE);
-                                textRoute.setVisibility(View.VISIBLE);
-                                textAR.setVisibility(View.GONE);
-                                textFacility.setVisibility(View.GONE);
-                                textSettings.setVisibility(View.GONE);
+                                if(currentMenu == item.getItemId()){
+                                    Toast.makeText(getApplicationContext(), "이미 고른 메뉴", Toast.LENGTH_SHORT).show();
+                                }
+                                else {
+                                    textHome.setVisibility(View.GONE);
+                                    textAR.setVisibility(View.GONE);
+                                    textFacility.setVisibility(View.GONE);
+                                    textSettings.setVisibility(View.GONE);
 
-                                fr = new RouteFragment();
+                                    fragment = new RouteFragment();
+                                    currentMenu = item.getItemId();
+                                    switchFragment();
+                                }
                                 break;
                             case R.id.action_AR:
-                                textHome.setVisibility(View.GONE);
-                                textRoute.setVisibility(View.GONE);
-                                textAR.setVisibility(View.VISIBLE);
-                                textFacility.setVisibility(View.GONE);
-                                textSettings.setVisibility(View.GONE);
-
-                                fr = new CarFragment();
+                                Toast.makeText(getApplicationContext(), "AR 기능 예정", Toast.LENGTH_SHORT).show();
                                 break;
                             case R.id.action_facility:
-                                textHome.setVisibility(View.GONE);
-                                textRoute.setVisibility(View.GONE);
-                                textAR.setVisibility(View.GONE);
-                                textFacility.setVisibility(View.VISIBLE);
-                                textSettings.setVisibility(View.GONE);
+                                if(currentMenu == item.getItemId()){
+                                    Toast.makeText(getApplicationContext(), "이미 고른 메뉴", Toast.LENGTH_SHORT).show();
+                                }
+                                else {
+                                    textHome.setVisibility(View.GONE);
+                                    textRoute.setVisibility(View.GONE);
+                                    textAR.setVisibility(View.GONE);
+                                    textFacility.setVisibility(View.VISIBLE);
+                                    textSettings.setVisibility(View.GONE);
 
-                                fr = new BusFragment();
+                                    fragment = new BusFragment();
+                                    currentMenu = item.getItemId();
+                                    switchFragment();
+                                }
                                 break;
                             case R.id.action_settings:
-                                textHome.setVisibility(View.GONE);
-                                textRoute.setVisibility(View.GONE);
-                                textAR.setVisibility(View.GONE);
-                                textFacility.setVisibility(View.GONE);
-                                textSettings.setVisibility(View.VISIBLE);
+                                if(currentMenu == item.getItemId()){
+                                    Toast.makeText(getApplicationContext(), "이미 고른 메뉴", Toast.LENGTH_SHORT).show();
+                                }
+                                else {
+                                    textHome.setVisibility(View.GONE);
+                                    textRoute.setVisibility(View.GONE);
+                                    textAR.setVisibility(View.GONE);
+                                    textFacility.setVisibility(View.GONE);
+                                    textSettings.setVisibility(View.VISIBLE);
 
-                                fr = new FootFragment();
+                                    fragment = new FootFragment();
+                                    currentMenu = item.getItemId();
+                                    switchFragment();
+                                }
                                 break;
                         }
-                        FragmentManager fm = getFragmentManager();
-                        FragmentTransaction fragmentTransaction = fm.beginTransaction();
-                        fragmentTransaction.replace(R.id.main_fragment_place, fr);
-                        fragmentTransaction.commit();
                         return true;
                     }
                 });
     }
 
-//    public void selectFrag(View view){
-//        Fragment fr;
-//
-//        if(view == findViewById(R.id.action_route)){
-//            fr = new RouteFragment();
-//        }
-//        else{
-//            fr = new RouteFragment();
-//        }
-//
-//        FragmentManager fm = getFragmentManager();
-//        FragmentTransaction fragmentTransaction = fm.beginTransaction();
-//        fragmentTransaction.replace(R.id.fragment_place, fr);
-//        fragmentTransaction.commit();
-//    }
+    public void switchFragment(){
+        fragmentManager = getFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main_fragment_place, fragment);
+        fragmentTransaction.commit();
+    }
 
+    @Override
+    public void onBackPressed() {
+        long tempTime = System.currentTimeMillis();
+        long intervalTime = tempTime - backPressedTime;
 
+        if (0 <= intervalTime && FINISH_INTERVAL_TIME >= intervalTime) {
+            super.onBackPressed();
+        }
+        else {
+            backPressedTime = tempTime;
+            Toast.makeText(getApplicationContext(), "'뒤로' 버튼을 한 번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
--- WelcomeSeoullo/app/src/main/java/com/example/jangwon/welcomeseoullo/RouteFragment.java
+++ WelcomeSeoullo/app/src/main/java/com/example/jangwon/welcomeseoullo/RouteFragment.java
@@ -1,15 +1,141 @@
 package com.example.jangwon.welcomeseoullo;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RouteFragment extends Fragment {
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+    View view;
+    View selectedFragment;
 
-        return inflater.inflate(R.layout.activity_path_info, container, false);
+    Fragment fr;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+        setHasOptionsMenu(true);
     }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+//        if(savedInstanceState == null){
+//            view = inflater.inflate(R.layout.activity_path_info, container, false);
+//
+//            final Button byCarButton = (Button) view.findViewById(R.id.byCar);
+//            final Button byBusButton = (Button) view.findViewById(R.id.byBus);
+//            final Button onFootButton = (Button) view.findViewById(R.id.onFoot);
+//            byCarButton.setTextColor(Color.parseColor("#FF0000"));
+//            byCarButton.setOnClickListener(new EditText.OnClickListener(){
+//                @Override
+//                public void onClick(View view) {
+//                    byCarButton.setTextColor(Color.parseColor("#FF0000"));
+//                    byBusButton.setTextColor(Color.parseColor("#000000"));
+//                    onFootButton.setTextColor(Color.parseColor("#000000"));
+//                    switchFragment(view);
+//                }
+//            });
+//            byBusButton.setOnClickListener(new EditText.OnClickListener(){
+//                @Override
+//                public void onClick(View view) {
+//                    byBusButton.setTextColor(Color.parseColor("#FF0000"));
+//                    byCarButton.setTextColor(Color.parseColor("#000000"));
+//                    onFootButton.setTextColor(Color.parseColor("#000000"));
+//                    switchFragment(view);
+//                }
+//            });
+//            onFootButton.setOnClickListener(new EditText.OnClickListener(){
+//                @Override
+//                public void onClick(View view) {
+//                    onFootButton.setTextColor(Color.parseColor("#FF0000"));
+//                    byBusButton.setTextColor(Color.parseColor("#000000"));
+//                    byCarButton.setTextColor(Color.parseColor("#000000"));
+//                    switchFragment(view);
+//                }
+//            });
+//            return view;
+//        }
+//        else{
+//            return selectedFragment;
+//        }
+        if(savedInstanceState == null){
+            view = inflater.inflate(R.layout.activity_path_info, container, false);
+
+            final Button byCarButton = (Button) view.findViewById(R.id.byCar);
+            final Button byBusButton = (Button) view.findViewById(R.id.byBus);
+            final Button onFootButton = (Button) view.findViewById(R.id.onFoot);
+            byCarButton.setTextColor(Color.parseColor("#FF0000"));
+            byCarButton.setOnClickListener(new EditText.OnClickListener(){
+                @Override
+                public void onClick(View view) {
+                    byCarButton.setTextColor(Color.parseColor("#FF0000"));
+                    byBusButton.setTextColor(Color.parseColor("#000000"));
+                    onFootButton.setTextColor(Color.parseColor("#000000"));
+                    switchFragment(view);
+                }
+            });
+            byBusButton.setOnClickListener(new EditText.OnClickListener(){
+                @Override
+                public void onClick(View view) {
+                    byBusButton.setTextColor(Color.parseColor("#FF0000"));
+                    byCarButton.setTextColor(Color.parseColor("#000000"));
+                    onFootButton.setTextColor(Color.parseColor("#000000"));
+                    switchFragment(view);
+                }
+            });
+            onFootButton.setOnClickListener(new EditText.OnClickListener(){
+                @Override
+                public void onClick(View view) {
+                    onFootButton.setTextColor(Color.parseColor("#FF0000"));
+                    byBusButton.setTextColor(Color.parseColor("#000000"));
+                    byCarButton.setTextColor(Color.parseColor("#000000"));
+                    switchFragment(view);
+                }
+            });
+        }
+
+        return view;
+
+    }
+
+    public void switchFragment(View view){
+        //Fragment fr;
+
+        if(view == view.findViewById(R.id.byCar)){
+            fr = new CarFragment();
+        }else if(view == view.findViewById(R.id.byBus)){
+            fr = new BusFragment();
+        }else{
+            fr = new FootFragment();
+        }
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_place, fr);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+    }
+
+
+
+
+
+
+
+
 }
