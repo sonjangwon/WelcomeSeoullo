a86e1be587f7b16a942724cb71e80ed081e9095d
손장원 <wlckd1031@sju.ac.kr>
2017-10-27 22:25:08 +0900

    Splash Video 오류 수정

--- WelcomeSeoullo/app/src/main/java/com/example/jangwon/welcomeseoullo/ARMenu/BlankFragment.java
+++ WelcomeSeoullo/app/src/main/java/com/example/jangwon/welcomeseoullo/ARMenu/BlankFragment.java
@@ -11,7 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
-import com.example.jangwon.welcomeseoullo.ARMenu.ARMainActivity;
+
 import com.example.jangwon.welcomeseoullo.MyVideoView;
 import com.example.jangwon.welcomeseoullo.R;
 
